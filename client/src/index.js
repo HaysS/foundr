5bb6ba07ae15e640897ac4b247664c114277fa39
Hays Stanford <stanfordhays@gmail.com>
2017-12-15 04:33:25 -0800

    Added bootstrap to package.json

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './css/index.css';
+
 import App from './containers/App';
 import registerServiceWorker from './utils/registerServiceWorker';
 
