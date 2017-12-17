a866821876e39c8e6d53822879083a9f35a973d6
Hays Stanford <stanfordhays@gmail.com>
2017-12-16 17:30:30 -0800

    Content columns now are full height so footer doesn't creep up

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -54,30 +54,32 @@ class App extends Component {
 				</nav>
 
 				{/*Main Content Area Wtih 2 Panels*/}
-				<div class="container-fluid">
-					{/*Left Column*/}
-					<div class="col-md-8">
-						{/*Main Showcase Area*/}
-						<div class="jumbotron">
-						  <h1 class="display-3">Meet Local Developers.</h1>
-						  <h2 class="display-3">Start Projects Together Now.</h2>
-						  <hr class="my-4" />
-						  <p>Connect your GitHub profile to show what you can do.</p>
-						  <p class="lead">
-						    <a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
-						  </p>
+				<div class="container-fluid h-100">
+					<div class="row justify-content-center h-100">
+						{/*Left Column*/}
+						<div class="col-md-8">
+							{/*Main Showcase Area*/}
+							<div class="jumbotron">
+							  <h1 class="display-3">Meet Local Developers.</h1>
+							  <h2 class="display-3">Start Projects Together Now.</h2>
+							  <hr class="my-4" />
+							  <p>Connect your GitHub profile to show what you can do.</p>
+							  <p class="lead">
+							    <a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
+							  </p>
+							</div>
 						</div>
-					</div>
 
-					{/*Right Column*/}
-					<div class="col-md-4">
-						<div class="panel panel-default">
-						  <div class="panel-heading">
-						    <h3 class="panel-title">Panel title</h3>
-						  </div>
-						  <div class="panel-body">
-						    Panel content
-						  </div>
+						{/*Right Column*/}
+						<div class="col-md-4">
+							<div class="panel panel-default">
+							  <div class="panel-heading">
+							    <h3 class="panel-title">Panel title</h3>
+							  </div>
+							  <div class="panel-body">
+							    Panel content
+							  </div>
+							</div>
 						</div>
 					</div>
 				</div>
