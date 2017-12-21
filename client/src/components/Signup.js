7abb1de007b4e442577bf60778a32420883b1f31
Hays Stanford <stanfordhays@gmail.com>
2017-12-21 13:48:27 -0800

    Added client side email and password validation for signups.

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,61 +2,110 @@ import React, { Component } from 'react';
 import '../css/custom.css';
 
 const dateFormat = require('dateformat');
+const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/;
+const passwordRegex = /^([a-zA-Z0-9_-]){8,24}$/
 
 class Signup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {email: '', password: ''};
+	constructor(props) {
+		super(props);
+		this.state = {email: '', password: '', submittedOnce: false};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-  handleChange(event) {
-  	const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+	handleChange(event) {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
 
-    this.setState({[name]: value});
-  }
+		this.setState({[name]: value});
+	}
 
-  handleSubmit(event) {
-    alert('Email of '+this.state.email+'and password of '+this.state.password+' have been submitted.');
-    event.preventDefault();
-  }
+	handleSubmit(event) {
+		event.preventDefault();
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-      	<div class="panel panel-default">
-      		<div class="panel-heading">
-				<h2 className="panel-title">Sign Up</h2>
-      		</div>
+		this.setState({submittedOnce: true})
 
-      		<div class="panel-body">
-		        <label>
-		          <input type="text" name="email"
-		          	placeholder="Email" 
-		          	pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" //Must be in email format
-		          	value={this.state.username} 
-		          	onChange={this.handleChange} 
-		          	required
-		          	/>
-		        </label>&nbsp;&nbsp;&nbsp;&nbsp;
-		        <label>
-		          <input type="password" name="password" 
-		          	placeholder="Password" 
-		          	pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" //Must contain at least 1 number, 1 uppercase letter, 1 lowercase letter, and 8 or more characters
-		          	value={this.state.password} 
-		          	onChange={this.handleChange} 
-		          	required
-		          	/>
-		        </label>&nbsp;&nbsp;&nbsp;&nbsp;
-		        <input class="btn btn-primary btn-sm" type="submit" value="Submit" />
-	        </div>
-        </div>
-      </form>
-    );
+		if(emailRegex.test(this.state.email) && passwordRegex.test(this.state.password))
+			alert('Email of '+this.state.email+'and password of '+this.state.password+' have been submitted.');
+
+	}
+
+	errorText(textElement) {
+		return(
+			<div>
+				{ textElement }
+			</div>
+		)
+	}
+
+  	validateEmail() {
+  		let textElement = <small>&nbsp;</small>
+
+  		if(this.state.email != '' && this.state.submittedOnce)
+  			if(!(emailRegex.test(this.state.email)))
+		  		textElement = <small class="text-danger">Must enter a valid email.</small>
+		  	else
+		  		textElement = <small class="text-success">Good to go!</small>
+			
+  		return(this.errorText(textElement))
+  	}
+
+  	validatePassword() {
+  		let textElement = <small>&nbsp;</small>
+
+  		if(this.state.password != '' && this.state.submittedOnce)
+			if(!passwordRegex.test(this.state.password))
+		  		textElement = <small class="text-danger">Must be 8 or more characters.</small>
+		  	else
+		  		textElement = <small class="text-success">Great password!</small>
+
+
+	  	return (this.errorText(textElement))
+  	}
+
+	render() {
+		return (
+		  <form onSubmit={this.handleSubmit}>
+		  	<div class="panel panel-default">
+		  		<div class="panel-heading">
+					<h2 className="panel-title">Sign Up</h2>
+		  		</div>
+
+		  		<div class="panel-body">
+			        <label>
+			          <input type="text" name="email"
+			          	placeholder="Email" 
+			          	value={this.state.email} 
+			          	onChange={this.handleChange} 
+			          	required
+			          	/>
+			          	{
+			          		this.validateEmail()
+			          	}
+			        </label>&nbsp;&nbsp;&nbsp;&nbsp;
+			        <label>
+						<input type="password" name="password" 
+							placeholder="Password" 
+							value={this.state.password} 
+							onChange={this.handleChange} 
+							required
+							/>
+						{
+							this.validatePassword()
+						}
+			        </label>&nbsp;&nbsp;&nbsp;&nbsp;
+			        <label>
+			        	<input class="btn btn-primary btn-sm" type="submit" value="Submit" />
+			        	{
+						 	this.errorText(<small>&nbsp;</small>)
+						}
+			        </label>
+		        </div>
+		    </div>
+		  </form>
+		);
   }
 }
 export default Signup;
